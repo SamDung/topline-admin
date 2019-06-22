ffc6bdc25619673692651b9b548ced72ae2736f4
shanda <982978726@qq.com>
2019-06-22 17:21:35 +0800

    添加视图组件并且配置路由导航

---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>apphome</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLogin',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>applogin</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHome',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
